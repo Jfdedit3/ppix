454c52493ac426e5c17928a9f0657ca227425a67
z4trox <kylian.lefebvre60@gmail.com>
2026-04-07 14:18:02 +0200

    Add Gradle settings from Pixiv-MultiPlatform

---
+++ settings.gradle.kts
@@ -0,0 +1,43 @@
+rootProject.name = "Pixiv-MultiPlatform"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        mavenLocal()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        maven("https://jogamp.org/deployment/maven")
+    }
+}
+
+include(":composeApp")
+
+include(":lib:epub")
+include(":lib:okio-enhancement-util")
+include(":lib:gif")
+include(":lib:file-picker")
+include(":lib:multiplatform-serializer-fix")
